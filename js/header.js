6d52087da8363516cd106ec67064afb0e7de54ec
jakbyte <jakbyte@gmail.com>
2019-12-29 13:54:56 -0500

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,23 +1,23 @@
-// false is inactive, true is active
-var navState = false;
+// Whether or not the small nav is enabled.
+let navState = false;
 
 function smallNav() {
-    // Toggle navState
-    navState = !navState
+    // Toggle navState.
+    navState = !navState;
 
     if (navState) {
-        // Show full-screen nav if enabled
-        document.getElementById("small-nav-screen").style.display = "block"
-        // Remove the inactive class
-        document.getElementById("small-nav").classList.remove("small-nav-inactive")
-        // Add the active class
-        document.getElementById("small-nav").classList.add("small-nav-active")
+        // Show fullscreen nav if it is enabled.
+        document.querySelector("#small-nav-screen").style.display = "block";
+        // Remove the inactive class.
+        document.querySelector("#small-nav").classList.remove("small-nav-inactive");
+        // Add the active class.
+        document.querySelector("#small-nav").classList.add("small-nav-active");
     } else {
-        // And hide it if un-toggling
-        document.getElementById("small-nav-screen").style.display = "none"
-        // Remove the active class
-        document.getElementById("small-nav").classList.remove("small-nav-active")
-        // Add the inactive class
-        document.getElementById("small-nav").classList.add("small-nav-inactive")
+        // Hide the small nav if it is disabled.
+        document.querySelector("#small-nav-screen").style.display = "none";
+        // Remove the active class.
+        document.querySelector("#small-nav").classList.remove("small-nav-active");
+        // Add the inactive class.
+        document.querySelector("#small-nav").classList.add("small-nav-inactive");
     }
 }

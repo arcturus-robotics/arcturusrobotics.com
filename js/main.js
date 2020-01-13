b348bbd3a7ea827152f1b27d4c80c940fb3f9c5f
jakbyte <jakbyte@gmail.com>
2020-01-13 18:42:43 -0500

    Delete more pesky tabs

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(() => {
 
 let FullscreenNavigation = class {
     constructor() {
-		this.state = false;
+        this.state = false;
         this.container = "#navigation-fullscreen";
         this.button = "#navigation-fullscreen-button";
     }
@@ -17,16 +17,16 @@ let FullscreenNavigation = class {
 
         if (this.state) {
             // Show the container.
-			$(this.container).toggleClass("navigation-fullscreen-container-active");
-			$(this.container).css("display", "block");
+            $(this.container).toggleClass("navigation-fullscreen-container-active");
+            $(this.container).css("display", "block");
             // Activate the button.
-			$(this.button).toggleClass("navigation-fullscreen-button-active")
+            $(this.button).toggleClass("navigation-fullscreen-button-active")
         } else {
-			// Hide the container.
-			$(this.container).toggleClass("navigation-fullscreen-container-active");
+            // Hide the container.
+            $(this.container).toggleClass("navigation-fullscreen-container-active");
             $(this.container).css("display", "none");
             // Deactivate the button.
-			$(this.button).toggleClass("navigation-fullscreen-button-active")
+            $(this.button).toggleClass("navigation-fullscreen-button-active")
         }
     }
 }
